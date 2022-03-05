2e935b3d93366e9b221f5f3a84040ba633d68294
fongziyjun16 <fongziyjun16@hotmail.com>
2022-03-05 10:19:35 -0500

    update

--- TBRBackendV1/src/main/java/com/cyj/tbr/controller/SearchController.java
+++ TBRBackendV1/src/main/java/com/cyj/tbr/controller/SearchController.java
@@ -1,7 +1,27 @@
 package com.cyj.tbr.controller;
 
+import com.cyj.tbr.entity.db.Item;
+import com.cyj.tbr.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class SearchController {
+
+    @Autowired
+    private GameService gameService;
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, List<Item>> search(@RequestParam(value = "game_id") String gameId) {
+        return gameService.searchItems(gameId);
+    }
+
 }
--- TBRBackendV1/src/main/java/com/cyj/tbr/service/GameService.java
+++ TBRBackendV1/src/main/java/com/cyj/tbr/service/GameService.java
@@ -1,5 +1,7 @@
 package com.cyj.tbr.service;
 
+import com.cyj.tbr.entity.db.Item;
+import com.cyj.tbr.entity.db.ItemType;
 import com.cyj.tbr.entity.response.Game;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,18 +17,23 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class GameService {
 
-    private static final String TOKEN = "nyyt7z9tzxz5yj8dna0k5bjzsd2j7o";
+    private static final String TOKEN = "c31jurj9be20vyr0ewom9riv38z1f2";
     private static final String CLIENT_ID = "drydljo27f1v4t3cfbjzmom1gr2ltk";
 
     private static final String TOP_GAME_URL = "https://api.twitch.tv/helix/games/top?first=%s";
     private static final String GAME_SEARCH_URL_TEMPLATE = "https://api.twitch.tv/helix/games?name=%s";
     private static final int DEFAULT_GAME_LIMIT = 20;
+
+    private static final String STREAM_SEARCH_URL_TEMPLATE = "https://api.twitch.tv/helix/streams?game_id=%s&first=%s";
+    private static final String VIDEO_SEARCH_URL_TEMPLATE = "https://api.twitch.tv/helix/videos?game_id=%s&first=%s";
+    private static final String CLIP_SEARCH_URL_TEMPLATE = "https://api.twitch.tv/helix/clips?game_id=%s&first=%s";
+    private static final String TWITCH_BASE_URL = "https://www.twitch.tv/";
+    private static final int DEFAULT_SEARCH_LIMIT = 20;
 
     private String buildGameURL(String url, String gameName, int limit) {
         if (gameName.equals("")) {
@@ -39,6 +46,15 @@ public class GameService {
             }
             return String.format(url, gameName);
         }
+    }
+
+    private String buildSearchURL(String url, String gameId, int limit) {
+        try {
+            gameId = URLEncoder.encode(gameId, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return String.format(url, gameId, limit);
     }
 
     private String searchTwitch(String url) throws TwitchException {
@@ -97,5 +113,67 @@ public class GameService {
             return gameList.get(0);
         }
         return null;
+    }
+
+    private List<Item> getItemList(String data) throws TwitchException{
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return Arrays.asList(mapper.readValue(data, Item[].class));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new TwitchException("Failed to parse item data from Twitch API");
+        }
+    }
+
+    private List<Item> searchStreams(String gameId, int limit) throws TwitchException {
+        List<Item> streams = getItemList(searchTwitch(buildSearchURL(STREAM_SEARCH_URL_TEMPLATE, gameId, limit)));
+        for (Item item : streams) {
+            item.setType(ItemType.STREAM);
+            item.setUrl(TWITCH_BASE_URL + item.getBroadcasterName());
+        }
+        return streams;
+    }
+
+    private List<Item> searchClips(String gameId, int limit) throws TwitchException {
+        List<Item> clips = getItemList(searchTwitch(buildSearchURL(CLIP_SEARCH_URL_TEMPLATE, gameId, limit)));
+        for (Item item : clips) {
+            item.setType(ItemType.CLIP);
+        }
+        return clips;
+    }
+
+    private List<Item> searchVideos(String gameId, int limit) throws TwitchException {
+        List<Item> videos = getItemList(searchTwitch(buildSearchURL(VIDEO_SEARCH_URL_TEMPLATE, gameId, limit)));
+        for (Item item : videos) {
+            item.setType(ItemType.VIDEO);
+        }
+        return videos;
+    }
+
+    public List<Item> searchByType(ItemType type, String gameId, int limit) throws TwitchException {
+        List<Item> items = Collections.emptyList();
+        switch (type) {
+            case STREAM:
+                items = searchStreams(gameId, limit);
+                break;
+            case CLIP:
+                items = searchClips(gameId, limit);
+                break;
+            case VIDEO:
+                items = searchVideos(gameId, limit);
+                break;
+        }
+        for (Item item : items) {
+            item.setGameId(gameId);
+        }
+        return items;
+    }
+
+    public Map<String, List<Item>> searchItems(String gameId) throws TwitchException {
+        Map<String, List<Item>> itemMap = new HashMap<>();
+        for (ItemType type : ItemType.values()) {
+            itemMap.put(type.toString(), searchByType(type, gameId, DEFAULT_SEARCH_LIMIT));
+        }
+        return itemMap;
     }
 }
