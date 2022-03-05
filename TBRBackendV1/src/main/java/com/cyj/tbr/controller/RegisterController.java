3e539de67531f3ddf585c1ca0d962c8a5fe06c35
fongziyjun16 <fongziyjun16@hotmail.com>
2022-03-05 12:35:57 -0500

    udpate register

---
+++ TBRBackendV1/src/main/java/com/cyj/tbr/controller/RegisterController.java
@@ -0,0 +1,15 @@
+package com.cyj.tbr.controller;
+
+import com.cyj.tbr.service.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RegisterController {
+
+    @Autowired
+    private RegisterService registerService;
+
+
+
+}
