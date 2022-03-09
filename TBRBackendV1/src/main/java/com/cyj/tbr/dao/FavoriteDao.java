3d9c3c670e834a1de655eae20e38414b0b7eee9a
fongziyjun16 <fongziyjun16@hotmail.com>
2022-03-08 23:12:10 -0500

    update

--- TBRBackendV1/src/main/java/com/cyj/tbr/dao/FavoriteDao.java
+++ TBRBackendV1/src/main/java/com/cyj/tbr/dao/FavoriteDao.java
@@ -1,10 +1,66 @@
 package com.cyj.tbr.dao;
 
+import com.cyj.tbr.entity.db.Item;
+import com.cyj.tbr.entity.db.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Repository
 public class FavoriteDao {
 
+    @Autowired
+    private SessionFactory sessionFactory;
 
+    public void setFavoriteItem(String userId, Item item) {
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            User user = session.get(User.class, userId);
+            user.getItemSet().add(item);
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public void unsetFavoriteItem(String userId, Item item) {
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            User user = session.get(User.class, userId);
+            user.getItemSet().remove(item);
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public Set<Item> getFavoriteItems(String userId) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(User.class, userId).getItemSet();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new HashSet<>();
+    }
 
 }
--- TBRBackendV1/src/main/java/com/cyj/tbr/service/FavoriteService.java
+++ TBRBackendV1/src/main/java/com/cyj/tbr/service/FavoriteService.java
@@ -1,13 +1,37 @@
 package com.cyj.tbr.service;
 
 import com.cyj.tbr.dao.FavoriteDao;
+import com.cyj.tbr.entity.db.Item;
+import com.cyj.tbr.entity.db.ItemType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 public class FavoriteService {
 
     @Autowired
     private FavoriteDao favoriteDao;
+
+    public void setFavoriteItem(String userId, Item item) {
+        favoriteDao.setFavoriteItem(userId, item);
+    }
+
+    public void unsetFavoriteItem(String userId, Item item) {
+        favoriteDao.unsetFavoriteItem(userId, item);
+    }
+
+    public Map<String, List<Item>> getFavoriteItems(String userId) {
+        Map<String, List<Item>> itemMap = new HashMap<>();
+        for (ItemType itemType : ItemType.values()) {
+            itemMap.put(itemType.toString(), new ArrayList<>());
+        }
+        Set<Item> favoriteItems = favoriteDao.getFavoriteItems(userId);
+        for (Item item : favoriteItems) {
+            itemMap.get(item.getType().toString()).add(item);
+        }
+        return itemMap;
+    }
 
 }
