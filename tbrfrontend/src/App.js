8132e8e7579c2424a4e24a3e8172323c0d17d8c8
fongziyjun16 <fongziyjun16@hotmail.com>
2022-03-20 18:28:36 -0400

    update

--- tbrfrontend/src/App.js
+++ tbrfrontend/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {Button, Layout, message} from 'antd';
-import {logout} from "./utils";
-import Login from "./components/Login";
-import Register from "./components/Register";
+import { Button, Col, Layout, message, Row } from 'antd';
+import Login from './components/Login';
+import Register from './components/Register';
+import { logout } from './utils';
+import Favorites from './components/Favorites';
 
 const { Header, Content, Sider } = Layout;
 
 class App extends React.Component {
-
     state = {
         loggedIn: false
     }
@@ -31,39 +31,49 @@ class App extends React.Component {
     }
 
     render = () => (
-      <Layout>
-        <Header>
-            {
-                this.state.loggedIn ?
-                    <Button shape="round" onClick={this.signoutOnClick}>
-                        Logout</Button> :
-                    (
-                        <>
-                            <Login onSuccess={this.signinOnSuccess} />
-                            <Register />
-                        </>
-                    )
-            }
-        </Header>
         <Layout>
-          <Sider width={300} className="site-layout-background">
-            {'Sider'}
-          </Sider>
-          <Layout style={{ padding: '24px' }}>
-            <Content
-                className="site-layout-background"
-                style={{
-                  padding: 24,
-                  margin: 0,
-                  height: 800,
-                  overflow: 'auto'
-                }}
-            >
-              {'Home'}
-            </Content>
-          </Layout>
+            <Header>
+                <Row justify="space-between">
+                    <Col>
+                        {
+                            this.state.loggedIn &&
+                            <Favorites />
+                        }
+                    </Col>
+                    <Col>
+                        {
+                            this.state.loggedIn ?
+                                <Button shape="round" onClick={this.signoutOnClick}>
+                                    Logout</Button> :
+                                (
+                                    <>
+                                        <Login onSuccess={this.signinOnSuccess} />
+                                        <Register />
+                                    </>
+                                )
+                        }
+                    </Col>
+                </Row>
+            </Header>
+            <Layout>
+                <Sider width={300} className="site-layout-background">
+                    {'Sider'}
+                </Sider>
+                <Layout style={{ padding: '24px' }}>
+                    <Content
+                        className="site-layout-background"
+                        style={{
+                            padding: 24,
+                            margin: 0,
+                            height: 800,
+                            overflow: 'auto'
+                        }}
+                    >
+                        {'Home'}
+                    </Content>
+                </Layout>
+            </Layout>
         </Layout>
-      </Layout>
     )
 }
 
